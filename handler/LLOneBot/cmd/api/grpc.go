eead63e3deebdbcdc95353c88ab73acf58b57bbe
nanachi-sh <nanachi.sh@protonmail.com>
2024-11-27 20:23:07 +0800

    update

--- handler/LLOneBot/cmd/api/grpc.go
+++ handler/LLOneBot/cmd/api/grpc.go
@@ -8,14 +8,22 @@ import (
 	"os"
 	"strconv"
 
+	request_pb "github.com/nanachi-sh/susubot-code/handler/LLOneBot/protos/handler/request"
 	response_pb "github.com/nanachi-sh/susubot-code/handler/LLOneBot/protos/handler/response"
+	"github.com/nanachi-sh/susubot-code/handler/LLOneBot/request"
 	"github.com/nanachi-sh/susubot-code/handler/LLOneBot/response"
 	"google.golang.org/grpc"
 )
 
-type responseService struct {
-	response_pb.ResponseHandlerServer
-}
+type (
+	responseService struct {
+		response_pb.ResponseHandlerServer
+	}
+
+	requestService struct {
+		request_pb.RequestHandlerServer
+	}
+)
 
 func GRPCServe() error {
 	portStr := os.Getenv("GRPC_LISTEN_PORT")
@@ -33,9 +41,9 @@ func GRPCServe() error {
 	if err != nil {
 		return err
 	}
-	respS := new(responseService)
 	gs := grpc.NewServer()
-	response_pb.RegisterResponseHandlerServer(gs, respS)
+	response_pb.RegisterResponseHandlerServer(gs, new(responseService))
+	request_pb.RegisterRequestHandlerServer(gs, new(requestService))
 	return gs.Serve(l)
 }
 
@@ -59,6 +67,40 @@ func (*responseService) BotResponseUnmarshal(ctx context.Context, req *response_
 			return
 		}
 		ret.data = response
+	}()
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case x := <-ch:
+		if x.err != nil {
+			return nil, x.err
+		}
+		return x.data, nil
+	}
+}
+
+func (*requestService) BotRequestMarshal(ctx context.Context, req *request_pb.BotRequestMarshalRequest) (*request_pb.BotRequestMarshalResponse, error) {
+	type d struct {
+		data *request_pb.BotRequestMarshalResponse
+		err  error
+	}
+	ch := make(chan *d, 1)
+	go func() {
+		ret := new(d)
+		defer func() { ch <- ret }()
+		requestH, err := request.New(req)
+		if err != nil {
+			ret.err = err
+			return
+		}
+		buf, err := requestH.Marshal()
+		if err != nil {
+			ret.err = err
+			return
+		}
+		ret.data = &request_pb.BotRequestMarshalResponse{
+			Buf: buf,
+		}
 	}()
 	select {
 	case <-ctx.Done():
