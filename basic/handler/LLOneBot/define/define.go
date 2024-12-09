51e0ed8e3a70160350d4ee78d30de15b210427cc
nanachi-sh <nanachi.sh@protonmail.com>
2024-12-09 22:11:12 +0800

    update

--- basic/handler/LLOneBot/define/define.go
+++ basic/handler/LLOneBot/define/define.go
@@ -28,33 +28,33 @@ func init() {
 	if gatewayHost == "" {
 		logger.Fatalln("Gateway API Host为空")
 	}
-	if ip := net.ParseIP(gatewayHost); ip != nil { //为IP
-		GatewayIP = ip
-		return
-	} else if ok, err := regexp.MatchString(`^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$`, gatewayHost); ok { //为域名
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-		defer cancel()
-		ips, err := net.DefaultResolver.LookupIP(ctx, "ip", gatewayHost)
-		if err != nil {
-			logger.Fatalln(err)
-			return
-		}
-		if len(ips) == 0 {
-			logger.Fatalln("Gateway API Host有误，无解析结果")
-			return
-		}
-		GatewayIP = ips[0]
-		return
-	} else { //若无错误，为未知
-		if err != nil {
-			logger.Fatalln(err)
-		} else {
-			logger.Fatalln("Gateway API Host有误，非域名或IP")
+	for {
+		if ip := net.ParseIP(gatewayHost); ip != nil { //为IP
+			GatewayIP = ip
+			break
+		} else if ok, err := regexp.MatchString(`^[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+$`, gatewayHost); ok { //为域名
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+			ips, err := net.DefaultResolver.LookupIP(ctx, "ip", gatewayHost)
+			if err != nil {
+				logger.Fatalln(err)
+			}
+			if len(ips) == 0 {
+				logger.Fatalln("Gateway API Host有误，无解析结果")
+			}
+			GatewayIP = ips[0]
+			break
+		} else { //若无错误，为未知
+			if err != nil {
+				logger.Fatalln(err)
+			} else {
+				logger.Fatalln("Gateway API Host有误，非域名或IP")
+			}
 		}
 	}
 	c, err := grpc.NewClient(fmt.Sprintf("%v:2080", GatewayIP.String()), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		panic(err)
+		logger.Fatalln(err)
 	}
 	GRPCClient = c
 	ConnectorCtx = metadata.NewOutgoingContext(context.Background(), metadata.New(map[string]string{
