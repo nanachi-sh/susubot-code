c663337f15764f569c8d6c0e406d10de4525f03f
nanachi-sh <nanachi.sh@protonmail.com>
2024-12-16 19:08:55 +0800

    update

--- plugin/TwoOnOne/LLOneBot/cmd/main.go
+++ plugin/TwoOnOne/LLOneBot/cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/nanachi-sh/susubot-code/plugin/TwoOnOne/LLOneBot/cmd/api"
+	"github.com/nanachi-sh/susubot-code/plugin/TwoOnOne/LLOneBot/log"
+)
 
+var logger = log.Get()
+
+func main() {
+	go func() {
+		if err := api.GRPCServe(); err != nil {
+			logger.Fatalln(err)
+		}
+	}()
+	select {}
 }
