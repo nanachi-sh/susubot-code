710692189ec7bd140b09713c089fe19e451c078a
nanachi-sh <nanachi.sh@protonmail.com>
2024-12-28 20:47:06 +0800

    update

--- basic/qqinteraction/qqinteraction/interaction.go
+++ basic/qqinteraction/qqinteraction/interaction.go
@@ -3613,6 +3613,24 @@ func uno(message *response_pb.Response_Message, text string) {
 			switch e := *resp.Err; e {
 			default:
 				logger.Printf("未处理异常：%v\n", e.String())
+			case uno_pb.Errors_PlayerNoExistAnyRoom:
+				if err := sendMessageToGroup(group.GroupId, []*request_pb.MessageChainObject{
+					&request_pb.MessageChainObject{
+						Type: request_pb.MessageChainType_MessageChainType_At,
+						At: &request_pb.MessageChain_At{
+							TargetId: senderid,
+						},
+					},
+					&request_pb.MessageChainObject{
+						Type: request_pb.MessageChainType_MessageChainType_Text,
+						Text: &request_pb.MessageChain_Text{
+							Text: " 报UNO失败，你不在任意一个桌内",
+						},
+					},
+				}); err != nil {
+					logger.Println(err)
+					return
+				}
 			case uno_pb.Errors_PlayerAlreadyCallUNO:
 				if err := sendMessageToGroup(group.GroupId, []*request_pb.MessageChainObject{
 					&request_pb.MessageChainObject{
