1ba943ab6c5effcb32cb8df44c3118b544c993ca
nanachi-sh <nanachi.sh@protonmail.com>
2024-12-28 20:46:11 +0800

    update

--- basic/qqinteraction/qqinteraction/interaction.go
+++ basic/qqinteraction/qqinteraction/interaction.go
@@ -437,9 +437,7 @@ func matchWhiteList(groupid string) bool {
 }
 
 func getText(mcs []*response_pb.MessageChainObject) string {
-	fmt.Println(len(mcs))
 	for _, v := range mcs {
-		fmt.Println(v)
 		if v.Type == response_pb.MessageChainType_MessageChainType_Text {
 			return v.Text.Text
 		}
@@ -3926,6 +3924,24 @@ func uno(message *response_pb.Response_Message, text string) {
 			switch e := *resp.Err; e {
 			default:
 				logger.Printf("未处理错误类型：%v\n", e.String())
+			case uno_pb.Errors_PlayerNoExistAnyRoom:
+				if err := sendMessageToGroup(group.GroupId, []*request_pb.MessageChainObject{
+					&request_pb.MessageChainObject{
+						Type: request_pb.MessageChainType_MessageChainType_At,
+						At: &request_pb.MessageChain_At{
+							TargetId: senderid,
+						},
+					},
+					&request_pb.MessageChainObject{
+						Type: request_pb.MessageChainType_MessageChainType_Text,
+						Text: &request_pb.MessageChain_Text{
+							Text: " 指出UNO失败，你不在任意一个桌内",
+						},
+					},
+				}); err != nil {
+					logger.Println(err)
+					return
+				}
 			case uno_pb.Errors_RoomNoSendingCard:
 				if err := sendMessageToGroup(group.GroupId, []*request_pb.MessageChainObject{
 					&request_pb.MessageChainObject{
