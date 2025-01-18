e87b08f6efe83617af280e68c65b56097aecdcf9
nanachi-sh <nanachi.sh@protonmail.com>
2025-01-18 18:42:57 +0800

    update

--- basic/fileweb/internal/web/web.go
+++ basic/fileweb/internal/web/web.go
@@ -8,11 +8,7 @@ import (
 	"github.com/nanachi-sh/susubot-code/basic/fileweb/internal/configs"
 )
 
-var (
-	LOAD struct{}
-)
-
-func init() {
+func Serve() {
 	fmt.Printf("Starting http file server at %d...\n", configs.HTTP_LISTEN_PORT)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", configs.HTTP_LISTEN_PORT), http.FileServer(http.Dir(configs.WebDir))))
 }
