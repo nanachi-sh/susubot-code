65f7bf5f29349ce842ef13403e7878d73c2c61fc
nanachi-sh <nanachi.sh@protonmail.com>
2025-01-22 22:17:06 +0800

    update

--- plugin/randomanimal/internal/configs/configs.go
+++ plugin/randomanimal/internal/configs/configs.go
@@ -134,6 +134,13 @@ func init() {
 	config := fmt.Sprintf(`Name: connector.rpc
 ListenOn: 0.0.0.0:%d
 Timeout: 10000`, GRPC_LISTEN_PORT)
+	if DEBUG {
+		config += `
+		Log: {
+			Mode: file, console
+			Path: out.log
+		}`
+	}
 	if err := os.WriteFile(RPCServer_Config, []byte(config), 0744); err != nil {
 		logger.Fatalln(err)
 	}
