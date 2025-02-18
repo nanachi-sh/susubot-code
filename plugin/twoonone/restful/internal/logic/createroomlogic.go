438b6a91de8783b0d833c5fa30c2e31a5480fdcc
nanachi-sh <nanachi.sh@protonmail.com>
2025-02-18 19:03:59 +0800

    update

--- plugin/twoonone/restful/internal/logic/createroomlogic.go
+++ plugin/twoonone/restful/internal/logic/createroomlogic.go
@@ -3,8 +3,9 @@ package logic
 import (
 	"context"
 
+	inside "github.com/nanachi-sh/susubot-code/plugin/twoonone/internal/twoonone"
+	"github.com/nanachi-sh/susubot-code/plugin/twoonone/pkg/protos/twoonone"
 	"github.com/nanachi-sh/susubot-code/plugin/twoonone/restful/internal/svc"
-	"github.com/nanachi-sh/susubot-code/plugin/twoonone/restful/internal/types"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -26,5 +27,5 @@ func NewCreateRoomLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Create
 func (l *CreateRoomLogic) CreateRoom() (resp any, err error) {
 	// todo: add your logic here and delete this line
 
-	return
+	return inside.NewAPIRequest(l.Logger).CreateRoom(&twoonone.CreateRoomRequest{})
 }
