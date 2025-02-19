2e314ac4e4a69065bb32320bebeef8414d01e29e
nanachi-sh <nanachi.sh@protonmail.com>
2025-02-19 19:12:04 +0800

    update

--- plugin/twoonone/restful/internal/handler/routes.go
+++ plugin/twoonone/restful/internal/handler/routes.go
@@ -14,7 +14,7 @@ import (
 func RegisterHandlers(server *rest.Server, serverCtx *svc.ServiceContext) {
 	server.AddRoutes(
 		rest.WithMiddlewares(
-			[]rest.Middleware{serverCtx.OIDCAuthMiddleware, serverCtx.ResponseHeaderMiddleware},
+			[]rest.Middleware{serverCtx.ResponseHeaderMiddleware, serverCtx.OIDCAuthMiddleware},
 			[]rest.Route{
 				{
 					Method:  http.MethodGet,
