d1352dedc6f4028e81ab39ccdaa3a60ea5580c35
nanachi-sh <nanachi.sh@protonmail.com>
2025-02-19 19:13:35 +0800

    update

--- plugin/twoonone/internal/middleware/header/middleware.go
+++ plugin/twoonone/internal/middleware/header/middleware.go
@@ -3,7 +3,7 @@ package header
 import "net/http"
 
 func Handle(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Origin", "http://localhost:8080")
 	w.Header().Add("Access-Control-Allow-Methods", "GET, POST")
 	next(w, r)
 }
