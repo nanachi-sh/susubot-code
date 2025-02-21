57fa3dae649220f7d5e528432ebfa37605e7692e
nanachi-sh <nanachi.sh@protonmail.com>
2025-02-21 18:24:28 +0800

    update

--- plugin/twoonone/restful/internal/types/types.go
+++ plugin/twoonone/restful/internal/types/types.go
@@ -3,67 +3,95 @@
 
 package types
 
-type Basic struct {
-	Name  string `custom:"name"`
-	Email string `custom:"email"`
-}
-
 type Card struct {
 	Number int `form:"number,range=[0:14]"`
 }
 
 type ExitRoomRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
-}
-
-type Extra struct {
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
 	WinCount  int     `custom:"wincount"`
 	LoseCount int     `custom:"losecount"`
 	Coin      float64 `custom:"coin"`
 }
 
 type GetDailyCoinRequest struct {
-	UserId string `form:"user_id"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type GetRoomRequest struct {
-	UI       UserInfo
-	RoomHash string `path:"room_hash"`
+	RoomHash  string  `path:"room_hash"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type JoinRoomRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type NoRobLandownerRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type NoSendCardRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type RobLandownerRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type SendCardRequest struct {
-	RoomHash  string `path:"room_hash"`
-	UserId    string `form:"user_id"`
-	SendCards []Card `form:"sendcards"`
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	SendCards []Card  `form:"sendcards"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
 
 type StartRoomRequest struct {
-	RoomHash string `path:"room_hash"`
-	UserId   string `form:"user_id"`
-}
-
-type UserInfo struct {
-	Basic Basic
-	Extra Extra
+	RoomHash  string  `path:"room_hash"`
+	UserId    string  `form:"user_id"`
+	Name      string  `custom:"name"`
+	Email     string  `custom:"email"`
+	WinCount  int     `custom:"wincount"`
+	LoseCount int     `custom:"losecount"`
+	Coin      float64 `custom:"coin"`
 }
