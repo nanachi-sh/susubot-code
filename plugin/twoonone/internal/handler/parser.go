f552f621acab8fdd9aecb659d6026170afc591a0
nanachi-sh <nanachi.sh@protonmail.com>
2025-02-21 18:36:53 +0800

    update

--- plugin/twoonone/internal/handler/parser.go
+++ plugin/twoonone/internal/handler/parser.go
@@ -25,6 +25,14 @@ var (
 
 func ParseCustom(r *http.Request, v any) error {
 	fmt.Println("in")
+	var (
+		email string
+		name  string
+
+		wincount  int
+		losecount int
+		coin      float64
+	)
 	// 检查是否有access_token
 	{
 		c, err := r.Cookie(types.COOKIE_KEY_access_token)
@@ -33,22 +41,14 @@ func ParseCustom(r *http.Request, v any) error {
 			token_raw := c.Value
 			m := jwt.MapClaims{}
 			jwt.ParseWithClaims(token_raw, m, nil)
-			email, ok := m["email"].(string)
+			ok := false
+			email, ok = m["email"].(string)
 			if !ok {
 				return pkg_types.NewError(twoonone.Error_ERROR_UNDEFINED, "从访问Token获取email失败")
 			}
-			name, ok := m["name"].(string)
+			name, ok = m["name"].(string)
 			if !ok {
 				return pkg_types.NewError(twoonone.Error_ERROR_UNDEFINED, "从访问Token获取name失败")
-			}
-			if err := customUnmarshaler.Unmarshal(
-				map[string]any{
-					types.PARSE_CUSTOM_KEY_email: email,
-					types.PARSE_CUSTOM_KEY_name:  name,
-				},
-				v,
-			); err != nil {
-				return err
 			}
 			fmt.Println("access ok")
 		}
@@ -65,17 +65,22 @@ func ParseCustom(r *http.Request, v any) error {
 		extra_raw = extra_raw[7:]
 		v := &types.JWT_EXTRA{}
 		jwt.ParseWithClaims(extra_raw, v, nil)
-		if err := customUnmarshaler.Unmarshal(
-			map[string]any{
-				types.PARSE_CUSTOM_KEY_wincount:  v.WinCount,
-				types.PARSE_CUSTOM_KEY_losecount: v.LoseCount,
-				types.PARSE_CUSTOM_KEY_coin:      v.Coin,
-			},
-			v,
-		); err != nil {
-			return err
-		}
+		wincount = v.WinCount
+		losecount = v.LoseCount
+		coin = v.Coin
 		fmt.Println("extra ok")
+	}
+	if err := customUnmarshaler.Unmarshal(
+		map[string]any{
+			types.PARSE_CUSTOM_KEY_email:     email,
+			types.PARSE_CUSTOM_KEY_name:      name,
+			types.PARSE_CUSTOM_KEY_wincount:  wincount,
+			types.PARSE_CUSTOM_KEY_losecount: losecount,
+			types.PARSE_CUSTOM_KEY_coin:      coin,
+		},
+		v,
+	); err != nil {
+		return err
 	}
 	fmt.Println("out")
 	return nil
