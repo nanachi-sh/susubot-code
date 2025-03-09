5282954bcb6f6d0595bc6a0ea0695f0be19e0b89
nanachi-sh <nanachi.sh@protonmail.com>
2025-03-09 21:23:48 +0800

    update

--- plugin/twoonone/stream/ws_handle.go
+++ plugin/twoonone/stream/ws_handle.go
@@ -2,6 +2,7 @@ package stream
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -82,10 +83,12 @@ func WebsocketHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			default:
 			}
+			fmt.Println("ping message sending")
 			if err := conn.WriteMessage(websocket.PingMessage, []byte("PING MESSAGE")); err != nil {
 				logger.Error(err)
 				return
 			}
+			fmt.Println("ping message ok")
 		}
 	}()
 	<-closed.Done()
