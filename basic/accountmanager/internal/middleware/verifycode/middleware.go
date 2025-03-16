822a2977aa13d3d4f1f931a8ef59392fa6b80d02
nanachi-sh <nanachi.sh@protonmail.com>
2025-03-16 21:44:21 +0800

    update

--- basic/accountmanager/internal/middleware/verifycode/middleware.go
+++ basic/accountmanager/internal/middleware/verifycode/middleware.go
@@ -1,6 +1,7 @@
 package verifycode
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/nanachi-sh/susubot-code/basic/accountmanager/internal/configs"
@@ -11,16 +12,21 @@ import (
 )
 
 func Handle(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println(r.Form, r.Form.Get("verify_id"), r.Form.Get("answer"))
+	fmt.Println("s1")
 	verify_id := r.Form.Get("verify_id")
+	fmt.Println("s2")
 	if verify_id == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	fmt.Println("s3")
 	answer := r.Form.Get("answer")
 	if answer == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	fmt.Println("s4")
 	if !configs.Captcha.Verify(verify_id, answer, false) {
 		ret, httpCode := handler.Generate(nil, types.NewError(accountmanager.Error_ERROR_VERIFYCODE_ANSWER_FAIL, ""))
 		httpx.WriteJsonCtx(r.Context(), w, httpCode, ret)
