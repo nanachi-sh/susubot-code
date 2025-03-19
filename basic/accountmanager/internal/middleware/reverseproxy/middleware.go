08424d30d2a6f90c857201a84c7f1384bdc7e40b
nanachi-sh <nanachi.sh@protonmail.com>
2025-03-19 17:38:32 +0800

    update

--- basic/accountmanager/internal/middleware/reverseproxy/middleware.go
+++ basic/accountmanager/internal/middleware/reverseproxy/middleware.go
@@ -11,18 +11,22 @@ import (
 
 func Handle(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	logger := logx.WithContext(r.Context())
+	logger.Info("in")
 	// API
 	if len(r.RequestURI) >= 3 && r.RequestURI[:3] == "/v1" {
 		w.Write([]byte("404 page not found\n"))
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+	logger.Info("s1")
 	u, err := url.Parse(configs.OIDC_ISSUER)
 	if err != nil {
 		logger.Error(err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	logger.Info("s2")
 	reverse := httputil.NewSingleHostReverseProxy(u)
 	reverse.ServeHTTP(w, r)
+	logger.Info("s3")
 }
